6e8cb9048a08b4d14c73cfc4c832c1fbdb1d091e
Manuel Duarte <carlosd99m@gmail.com>
2021-10-04 15:44:37 -0600

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket connection;
+
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6400);
+        }
+    }
+}
